1d679d1006b5d17a1e6c7701ed91c01ad4ffde38
SBICZA01\A253638 <rivaldo.mkhola@standardbank.co.za>
2023-12-05 22:50:06 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        //This method below called a constructor, is is called by default when a book object is created
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("harry Potter","Jk Rowling", 400);
+
+            Console.WriteLine(book1);
+            Console.ReadLine();
+        }
+    }
+}
